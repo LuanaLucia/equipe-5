87a4dbede9e16b772cc21e1db20b06e20606f678
LuanaLucia <luanasantos@outlook.com>
2023-05-12 23:23:39 -0300

    ajuste api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-// Sinalizar uso de Json
-app.use(express.json())
+app.use(cors());
+  // Sinalizar uso de Json
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello Word!");
@@ -151,6 +151,7 @@ app.get("/itens/:modelo", (req, res) => {
   const modelo = req.params.modelo;
 
   const item = itens.data.filter(function (a) {
+
     return a.modelo === modelo;
   });
   res.send(item);
